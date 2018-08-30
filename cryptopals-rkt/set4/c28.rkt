dfab5992425d0fc833760c48e29540cca9c5c292
Austin Sparks <aasparks93@gmail.com>
2018-08-30 16:13:32 -0400

    Done with c28.rkt

---
+++ cryptopals-rkt/set4/c28.rkt
@@ -0,0 +1,27 @@
+#lang racket
+
+; Challenge 28
+;; Implement a SHA-1 keyed MAC
+
+;;; Find a SHA-1 implementation in the language
+;;; you code in. (I made it).
+(require "../sha1/sha1.rkt"
+         "../set1/c1.rkt"
+         racket/random)
+
+(provide hmac)
+
+;;; Write a function to authenticate a message
+;;; under a secret key by using a secret-prefix
+;;; MAC, which is simply:
+;;;    SHA1(key || message)
+(define KEY (crypto-random-bytes 16))
+
+(define (hmac msg)
+  (sha-1 (bytes-append KEY msg)))
+
+;;; Verify you cannot tamper with the message without breaking
+;;; the MAC you've produced, and that you can't produce a new
+;;; MAC without knowing the key.
+(module+ test
+  (ascii->hex (hmac #"Attack at dawn!")))
